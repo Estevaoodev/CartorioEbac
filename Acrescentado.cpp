8fed3b6c9dbf890b3bc1dbc3461db3797e01e777
Estevaoodev <oestevaosantosp@gmail.com>
2024-07-27 13:00:50 -0300

    Acrescentado

--- Acrescentado.cpp
+++ Acrescentado.cpp
@@ -87,7 +87,7 @@ int consulta()
 	    printf("\n\n");//pular linha no sistema
 	    
 	 }
-	 
+	  fclose(file);
 	 system("pause");
 }
 
@@ -95,7 +95,7 @@ int deletar()
 {
  	char cpf[40];
  	
- 	printf("Digite o CPF do usuário a ser deletado: ");
+ 	printf("Digite o CPF do usuário a ser deletado: ");//recebendo qual usuário sera deletado0
  	scanf("%s", cpf);
  	
  	remove(cpf);
@@ -108,61 +108,79 @@ int deletar()
  		printf("O usuário não se encontra no sistema! \n");
  		system("pause"); 		
 	 }
-	    
+	    fclose(file); 
 }
 
 int main()
 {
 	int opcao=0; //Definindo variáveis
 	int laco=1;
+	char senhadigitada[10] ="a";
+	int comparacao;
+	setlocale(LC_ALL, "Portuguese");
 	
-	for(laco=1;laco=1;)
-    {
+	printf("### Cartório da EBAC ###\n\n");
+	printf("Login de admistrador!\n\n Digite a sua senha:");
+	scanf("%s",senhadigitada);
+	
+	comparacao = strcmp(senhadigitada, "admin");
+	
+         
+        if (comparacao == 0)
+           {
+		       
+			 
+		    
+                  system("cls");	
+	              for(laco=1;laco=1;)
+                 {
 		 
 	
-         system("cls");  
+                 system("cls");  
 	
-	     setlocale(LC_ALL, "Portuguese");//Definindo linguagem
-	
-	     printf("### Cartório da EBAC ###\n\n");//Início do menu
-	     printf("Escolha a opção do menu desejada: \n\n");
-	     printf("\t1 - Registar nomes\n");
-	     printf("\t2 - Consultar nome\n");
-	     printf("\t3 - Deletar nomes \n\n");
-	     printf("\t4 - Sair do sistema");
-         printf("Opção: ");//final do menu
+	              setlocale(LC_ALL, "Portuguese");//Definindo linguagem
+	 
+	             printf("### Cartório da EBAC ###\n\n");//Início do menu
+	             printf("Escolha a opção do menu desejada: \n\n");
+	              printf("\t1 - Registar nomes\n");
+	             printf("\t2 - Consultar nome\n");
+	             printf("\t3 - Deletar nomes \n");
+	             printf("\t4 - Sair do sistema\n");
+                 printf("Opção: ");//final do menu
 
 
-         scanf("%d", &opcao);//armezenando a escolha do usuário
+                 scanf("%d", &opcao);//armezenando a escolha do usuário
           
-         system("cls"); //responsavel por limpar a tela
+                 system("cls"); //responsavel por limpar a tela
          
-             switch(opcao)//inicio da sessão do menu
-             {
-             case 1:
-             registro();
-    	     break;
+                 switch(opcao)//inicio da sessão do menu
+                 {
+                 case 1:
+                 registro();
+    	         break;
 		 
-		     case 2:
-		     consulta();
-		     break;
+		         case 2:
+		         consulta();
+		         break;
 		  
-		     case 3:
-		     deletar();
-		     break;
+		         case 3:
+		         deletar();
+		         break;
 		     
-		     case 4:
-		     printf("Obrigado por utilizar o sistema");
-		     return 0;
-		     break;
+		         case 4:
+		         printf("Obrigado por utilizar o sistema");
+		         return 0;
+		         break;
 		  
-		     default:
-		     printf("Essa opção não esta disponível!\n");
-		     system("pause");//fim da seleção
-		     break;
-		     }
-         
-         
-         
-     }
-}
+		         default:
+		         printf("Essa opção não esta disponível!\n");
+		         system("pause");//fim da seleção
+		         break;
+		         }
+                  }
+            }    
+                    
+            else
+			printf("Senha incorreta!");      
+ }    
+
